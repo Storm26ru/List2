c6fc2d800a73a0492e09c90557d72e0686cfad4a
Storm <Storm@STORMSSD>
2024-09-24 01:38:13 +0300

    Add project files.

---
+++ List2/main.cpp
@@ -0,0 +1,129 @@
+#include <iostream>
+using namespace std;
+
+#define tab "\t"
+#define delimiter "\n----------------------------------\n"
+
+class List
+{
+	class Element
+	{
+		int Data;
+		Element* pPrev;
+		Element* pNext;
+
+	public:
+		Element(int Data, Element* pPrev=nullptr, Element* pNext = nullptr) : Data(Data), pPrev(pPrev), pNext(pNext) { cout << "EConstructor:\t" << this << endl; }
+		~Element() { cout << "EDestructor:\t" << this << endl; }
+		friend class List;
+	} *Head,*Tail;
+	size_t size;
+public:
+	List()
+	{
+		Head = Tail = nullptr;
+		size = 0;
+		cout << "LConstructor:\t" << this << endl;
+	}
+	~List() { cout << "LDestructor:\t" << this << endl; }
+
+	//						Adding elements:
+	void push_front(int Data)
+	{
+		if (Head == nullptr && Tail == nullptr) Head = Tail = new Element(Data);
+		else
+		{
+			Element* New = new Element(Data);
+			New->pNext = Head;
+			Head->pPrev = New; 
+			Head = New;
+			
+		}
+		size++;
+
+	}
+	void push_back(int Data)
+	{
+		if (Head == nullptr && Tail == nullptr) Head = Tail = new Element(Data);
+		else
+		{
+			Element* New = new Element(Data);
+			New->pPrev = Tail;
+			Tail->pNext = New;
+			Tail = New;
+		}
+		size++;
+	}
+	void pop_front()
+	{
+		if (Head == nullptr && Tail == nullptr) return;
+		if (Head->pNext)
+		{
+			Head = Head->pNext;
+			Head->pPrev = nullptr;
+		}
+		else Head = Tail = nullptr;
+		size--;
+	}
+	void pop_back()
+	{
+		if (Head == nullptr && Tail == nullptr) return;
+		if (Tail->pPrev)
+		{
+			Tail = Tail->pPrev;
+			Tail->pNext = nullptr;
+		}	
+		else Head = Tail = nullptr;
+		size--;
+	}
+	void insert(int index)
+	{
+
+	}
+
+
+	//						Methods:
+	void print()const
+	{
+		cout << "Head:\t" << Head << endl;
+		for (Element* Temp = Head; Temp; Temp = Temp->pNext)
+			cout << Temp->pPrev << tab << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
+		cout << "Tail:\t" << Tail << endl;
+		cout << "Количество элементов списка: " << size << endl;
+	}
+	void revers_print()const
+	{
+		cout << "Tail:\t" << Tail << endl;
+		for (Element* Temp = Tail; Temp; Temp = Temp->pPrev)
+			cout << Temp->pPrev << tab << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
+		cout << "Head:\t" << Head << endl;
+		cout << "Количество элементов списка: " << size << endl;
+
+	}
+
+
+
+};
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	int n;
+	cout << "Введите размер списка: "; cin >> n;
+	List list;
+	for (int i = 0; i < n; i++)
+	{
+		list.push_back(rand() % 100);
+	}
+	list.print();
+	list.pop_front();
+	list.print();
+	list.pop_back();
+	list.print();
+	
+	
+	
+
+
+}
+
