17a8f58e52f142c6f7caa00c41386e816952874e
Storm <Storm@STORMSSD>
2024-09-27 23:33:44 +0300

    file

--- List2/List.h
+++ List2/List.h
@@ -1,1 +1,202 @@
 #pragma once
+#include <iostream>
+#include<initializer_list>
+using namespace std;
+
+#define tab "\t"
+#define delimiter "\n----------------------------------\n"
+
+template<typename T> class List
+{
+	class Element
+	{
+		T Data;
+		Element* pPrev;
+		Element* pNext;
+
+	public:
+		Element(T Data, Element* pPrev = nullptr, Element* pNext = nullptr) : Data(Data), pPrev(pPrev), pNext(pNext) { cout << "EConstructor:\t" << this << endl; }
+		~Element() { cout << "EDestructor:\t" << this << endl; }
+		friend class List;
+	} *Head, * Tail;
+	class Iterator ///????????????????????????????
+	{
+		Element* ptr;
+	public:
+		Iterator(Element* ptr) : ptr(ptr) {}
+		Iterator operator++()
+		{
+			Element* temp = ptr;
+			ptr = ptr->pNext;
+			return Iterator(temp);
+		}
+		bool operator !=(const Iterator& li)const { return ptr != li.ptr; }
+		const T& operator *()const { return ptr->Data; }
+
+	};
+	size_t size;
+public:
+	List()
+	{
+		Head = Tail = nullptr;
+		size = 0;
+		cout << "LConstructor:\t" << this << endl;
+	}
+	List(initializer_list<T>l)
+	{
+		for (T data : l)
+		{
+			push_back(data);
+		}
+	}
+	List(const List& other) :List() //???????????????????
+	{
+		//for (int i : other) push_back(i);
+		*this = other;
+	}
+	~List() { while (Head)pop_front(); cout << "LDestructor:\t" << this << endl; }
+
+	//						Adding elements:
+	void push_front(T Data)
+	{
+		if (Head == nullptr && Tail == nullptr) Head = Tail = new Element(Data);
+		else
+		{
+			Element* New = new Element(Data);
+			New->pNext = Head;
+			Head->pPrev = New;
+			Head = New;
+
+		}
+		size++;
+
+	}
+	void push_back(T Data)
+	{
+		if (Head == nullptr && Tail == nullptr) Head = Tail = new Element(Data);
+		else
+		{
+			Element* New = new Element(Data);
+			New->pPrev = Tail;
+			Tail->pNext = New;
+			Tail = New;
+		}
+		size++;
+	}
+	void pop_front()
+	{
+		if (Head == nullptr && Tail == nullptr) return;
+		if (Head->pNext)
+		{
+			Head = Head->pNext;
+			delete Head->pPrev;
+			Head->pPrev = nullptr;
+		}
+		else { delete Head; Head = Tail = nullptr; }
+		size--;
+	}
+	void pop_back()
+	{
+		if (Head == nullptr && Tail == nullptr) return;
+		if (Tail->pPrev)
+		{
+			Tail = Tail->pPrev;
+			delete Tail->pNext;
+			Tail->pNext = nullptr;
+		}
+		else { delete Tail; Head = Tail = nullptr; }
+		size--;
+	}
+	void insert(T Data, int index)
+	{
+		if (Head == nullptr && Tail == nullptr) return;
+		if (index == 0)push_front(Data);
+		if (index == size)push_back(Data);
+		else
+		{
+			Element* New = new Element(Data);
+			Element* Temp;
+			if (index < size / 2)
+			{
+				Temp = Head;
+				for (int i = 0; i < index; i++) Temp = Temp->pNext;
+			}
+			else
+			{
+				Temp = Tail;
+				for (int i = size; i > index + 1; i--)Temp = Temp->pPrev;
+			}
+			New->pNext = Temp;
+			New->pPrev = Temp->pPrev;
+			Temp->pPrev->pNext = New;
+			Temp->pPrev = New;
+			size++;
+		}
+	}
+	void erase(int index)
+	{
+		Element* Temp;
+		if (Head == nullptr && Tail == nullptr || index >= size) return;
+		if (index == size - 1) { pop_back(); return; }
+		if (index == 0)pop_front();
+		else
+		{
+			if (index < size / 2)
+			{
+				Temp = Head;
+				for (int i = 0; i < index; i++) Temp = Temp->pNext;
+			}
+			else
+			{
+				Temp = Tail;
+				for (int i = size; i > index + 1; i--)Temp = Temp->pPrev;
+			}
+			Temp->pPrev->pNext = Temp->pNext;
+			Temp->pNext->pPrev = Temp->pPrev;
+			delete Temp;
+			size--;
+		}
+
+	}
+	Iterator begin()const { return Iterator(Head); }
+	Iterator end()const { return Iterator(nullptr); }
+
+
+	//						Methods:
+	void print()const
+	{
+		cout << "Head:\t" << Head << endl;
+		for (Element* Temp = Head; Temp; Temp = Temp->pNext)
+			cout << Temp->pPrev << tab << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
+		cout << "Tail:\t" << Tail << endl;
+		cout << "Количество элементов списка: " << size << endl;
+	}
+	void revers_print()const
+	{
+		cout << "Tail:\t" << Tail << endl;
+		for (Element* Temp = Tail; Temp; Temp = Temp->pPrev)
+			cout << Temp->pPrev << tab << Temp << tab << Temp->Data << tab << Temp->pNext << endl;
+		cout << "Head:\t" << Head << endl;
+		cout << "Количество элементов списка: " << size << endl;
+
+	}
+	//						Operators:
+	List& operator=(const List& other)
+	{
+		if (this == &other)return *this;
+		while (Head)pop_front();
+		for (T i : other)push_back(i);
+		return *this;
+	}
+
+
+
+};
+
+template<typename T> List<T>  operator + (const List<T>& left, const List<T>& right)
+{
+	List<T> temp = left;
+	for (T i : right)temp.push_back(i);
+	return temp;
+}
+
