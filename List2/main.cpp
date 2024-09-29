03d208daf4690699b0b188c6c043d60d5a16cf58
Storm <Storm@STORMSSD>
2024-09-29 12:35:14 +0300

    done

--- List2/main.cpp
+++ List2/main.cpp
@@ -6,7 +6,7 @@ void main()
 	List<int> list1 = { 3, 5, 8, 13, 21 };
 	List<int> list2 = { 34, 55, 89 };
 	List<int> list3 = list1 + list2;
-	for (auto i : list3)
+	for (int i : list3)
 	{
 		cout << i << tab;
 	}
